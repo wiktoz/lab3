406a546626a249d596fbf1110ce4cab50e847414
wiktoz <wiciazaw@wp.pl>
2024-12-18 23:57:03 +0100

    all

--- models/lesson.py
+++
@@ -1,9 +0,0 @@
-from extensions import db
-from uuid import uuid4
-
-class Lesson(db.Model):
-    lesson_id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid4()))
-    student_id = db.Column(db.String(36), db.ForeignKey('students.student_id'), nullable=False)
-    teacher_id = db.Column(db.String(36), db.ForeignKey('teachers.teacher_id'), nullable=False)
-    subject_id = db.Column(db.String(36), db.ForeignKey('subjects.subject_id'), nullable=False)
-    datetime = db.Column(db.DateTime, nullable=False)
